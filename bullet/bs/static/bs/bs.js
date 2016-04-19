2a316995a70386d939a4a7feec691ce72dc46563
BoseSean <xinyesg@gmail.com>
2016-04-19 23:14:30 +0800

    daily commit

--- bullet/bs/static/bs/bs.js
+++ bullet/bs/static/bs/bs.js
@@ -1,4 +1,9 @@
-var p = document.getElementById('ab');
-while(1){
-p.toggle(3000);
-} 
+var p = $('#ab');
+// while(0){
+// p.toggle(3000);
+// } 
+var jqxhr = $.getJSON('/api/',{
+    dataType: 'json'
+}).done(function (data) {
+    // data已经被解析为JSON对象了
+});
